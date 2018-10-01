5e0b31134e6f8c8c5f6cd4cf40c7273a1d28427c
Naveen Sankabathula <naveen.c.s@hotmail.com>
2018-10-01 18:04:53 -0400

    Update env-setup.sh

--- env-setup.sh
+++ env-setup.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-conda install pip
-#pip install --upgrade pip
-conda update --name en-nlp pip
 echo "Installing using requirement file: $1"
 while read requirement; do conda install --yes $requirement || pip install $requirement; done < $1
