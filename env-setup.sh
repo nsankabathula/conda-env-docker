8d1c06c4befc2b6d33269c483e49ea37bde3f8c0
Naveen Sankabathula <naveen.c.s@hotmail.com>
2018-10-01 18:00:49 -0400

    Update env-setup.sh

--- env-setup.sh
+++ env-setup.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-sudo apt-get install gcc
-echo "Creating conda env.."
-conda create --yes --name en-nlp
-echo "Activating conda env.."
-source activate en-nlp
-echo "Installing & Upgrading pip.."
 conda install pip
 #pip install --upgrade pip
 conda update --name en-nlp pip
