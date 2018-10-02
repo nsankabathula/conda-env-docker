3a0647fbbe257e343bfac8d63ca2079b674532a2
Naveen Sankabathula <naveen.c.s@hotmail.com>
2018-10-01 20:28:41 -0400

    Update env-setup.sh

--- env-setup.sh
+++ env-setup.sh
@@ -3,4 +3,4 @@
 apt-get install gcc
 echo "Installing using requirement file: $1"
 conda update pip
-while read requirement; do conda install --yes $requirement || pip install $requirement; done < $1
+while read requirement; do echo "Installing $requirement" conda install --yes $requirement || pip install $requirement; done < $1
