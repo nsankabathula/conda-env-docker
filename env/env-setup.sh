4de8df19bbf40ebf39fe60f7d82d44904ea2d60e
Naveen Sankabathula <naveen.c.s@hotmail.com>
2018-10-02 16:54:43 -0400

    Update env-setup.sh

--- env/env-setup.sh
+++ env/env-setup.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 echo "Installing using requirement file: $1"
-pip install --upgrade pip
 while read requirement; do conda install --yes $requirement || pip install $requirement; done < $1
 
 conda install --quiet --yes -c conda-forge gensim
