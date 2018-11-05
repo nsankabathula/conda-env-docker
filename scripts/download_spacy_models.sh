f377dd515abb93ad2a968d103092bc5e62435621
Naveen Sankabathula <naveen.c.s@hotmail.com>
2018-11-05 11:55:19 -0500

    Update download_spacy_models.sh

--- scripts/download_spacy_models.sh
+++ scripts/download_spacy_models.sh
@@ -5,9 +5,9 @@ then
   source activate $1
 fi
 
-python -m spacy download en_core_web_lg
+# python -m spacy download en_core_web_lg
 #python -m spacy download en_core_web_sm
 
-#python -m spacy download en_core_web_md
+python -m spacy download en_core_web_md
 
 
